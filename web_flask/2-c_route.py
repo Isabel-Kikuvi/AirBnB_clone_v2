4f6c4746c2c65ccc068e1267e5d74b1cf3feb490
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-09-22 06:43:33 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c():
+def c(text):
     """Returns a given text and replaces _with "" """
     return ("C {}".format(text.replace("_", " ")))
 
