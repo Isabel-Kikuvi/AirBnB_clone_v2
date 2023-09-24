2b4abf27b40a1425ff19705f7196fb9418b0b365
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-09-24 09:36:47 -0700

    edits

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
-from flask import Flask, render_template
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
