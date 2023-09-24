5d3b4f11329f9d00a9f5f473626b9c8ea21335a5
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-09-24 10:32:02 -0700

    edits

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,6 +13,7 @@ def cities_by_states():
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def close(self):
     """Closes the current SQLAlchemy Session."""
