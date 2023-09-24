d4797bdf226b3252c6bc1b7d0103f28fd9eed359
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-09-24 10:21:41 -0700

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display a HTML page"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+@app.teardown_appcontext
+def close(self):
+    """Closes the current SQLAlchemy Session."""
+    storage.close()
